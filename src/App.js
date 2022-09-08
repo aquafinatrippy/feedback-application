acae607a7ab90993a44722085d00f355c3207b80
unknown <taneldeviis@hotmail.com>
2022-09-08 20:22:55 +0300

    default data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
+import { useState } from "react";
+import FeedbackList from "./components/FeedbackList";
 import Header from "./components/Header";
+import FeedbackData from "./data/FeedbackData";
 
 const App = () => {
+  const [feedback, setFeedback] = useState(FeedbackData);
   return (
     <div>
       <Header text="Feedback Application" />
       <div className="container">
-        <h1>my app</h1>
+        <FeedbackList feedback={feedback} />
       </div>
     </div>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,11 @@
-const Header = ({ text }) => {
+const Header = ({ text, bgColor, textColor }) => {
+  const headerStyles = {
+    backgroundColor: bgColor,
+    color: textColor,
+  };
+
   return (
-    <header>
+    <header style={headerStyles}>
       <div className="container">
         <h2>{text}</h2>
       </div>
@@ -10,6 +15,8 @@ const Header = ({ text }) => {
 
 Header.defaultProps = {
   text: "Feedback Application",
+  bgColor: "rgba(0,0,0, 0.4)",
+  textColor: "#ff6a95",
 };
 
 export default Header;
